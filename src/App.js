562083f184dc4a52b2d4d127692ee8347ecabc61
Charlie Gilman <charliegilmanuk@gmail.com>
2022-03-07 23:15:04 +0000

    Add shop component with collections

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
 import {Homepage} from "./pages/homepage/homepage.component";
 import {Route} from "react-router-dom";
-
-const HatsPage = () => (
-    <div className="hats-components">
-        <h1>Hats Page</h1>
-    </div>
-)
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
         <Route exact path="/" component={Homepage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
